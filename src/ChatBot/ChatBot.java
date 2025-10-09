e16fa6ef2c2b8cec4afe6491650f1dfa15374b0f
Mllkssaa <k.v.samokhatka@student.knai.edu>
2025-10-09 20:35:51 +0300

    chatBot {2-nd} stage

--- src/ChatBot/ChatBot.java
+++ src/ChatBot/ChatBot.java
@@ -1,9 +1,18 @@
+import java.util.Scanner;
+
 public class ChatBot {
     public static void main(String[] args) {
-        String bot_name = "Bot";
+        String bot_name = "Akaza";
         int birth_year = 2025;
 
         System.out.println("Hello! My name is " + bot_name);
-        System.out.println("I was created" + birth_year);
+        System.out.println("I was created in " + birth_year);
+        System.out.println("Please, remind me your name");
+
+        Scanner scanner =  new Scanner(System.in);
+        String your_name = scanner.nextLine();
+
+
+        System.out.println("What a great name you have, " + your_name);
     }
 }
