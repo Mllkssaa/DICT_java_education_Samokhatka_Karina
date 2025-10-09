220a4ccb41882949328098d1e91b451a11ec94bf
Mllkssaa <k.v.samokhatka@student.knai.edu>
2025-10-09 20:25:44 +0300

    chatBot {1-st} stage

---
+++ src/ChatBot/ChatBot.java
@@ -0,0 +1,9 @@
+public class ChatBot {
+    public static void main(String[] args) {
+        String bot_name = "Bot";
+        int birth_year = 2025;
+
+        System.out.println("Hello! My name is " + bot_name);
+        System.out.println("I was created" + birth_year);
+    }
+}
